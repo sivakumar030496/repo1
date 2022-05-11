5c9b9e4e8efa3409d3ab384e6ec3987c49ee9d17
Siva <sivakumarvaddipalli@gmail.com>
2022-05-11 10:17:26 +0530

    count of charecters

--- practise.py
+++ practise.py
@@ -3034,5 +3034,14 @@ import re
 #     if t-i in s:
 #         print(t-i,i)
 
-
+n="xxx yyy zzz xxx"
+d=dict()
+for i in n:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]+=1
+# print(d)
+for k,v, in d.items():
+    print(k,v, sep=":")
 
