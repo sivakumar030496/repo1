e63800eb3afc32d0a3736e9a23a06c6af48d0dee
Siva <sivakumarvaddipalli@gmail.com>
2022-09-15 14:58:46 +0530

    practices_programs

---
+++ python1.py
@@ -0,0 +1,287 @@
+# l=[]
+# for i in range(2000 ,3201):
+#     if (i%7==0) and (i%5!=0):
+#         l.append(str(i))
+# #print(l,end=",")
+# #
+# str=("rajeghegsjh")
+# #print(len(str))
+# #
+# str="12344"
+# print(int(str))
+# #print(float(str))
+# #
+# l=[1,2,4,5,6,7,8,9]
+# print(l)
+# #print(tuple(l))
+# t=(1,2,3,4,5,66,7,8,9)
+# print(t)
+# #print(list(t))
+# dict={"name":"raji","age":"24","year":"1997"}
+# print(dict)
+# print(dict.keys())
+# print(dict.values())
+# #
+# l=[11,32,43,44,55,66,76]
+# print(l)
+# l[2]=95
+# print(l)
+# t=(11,23,44,55,66,77)
+# print(t)
+# #
+# #
+# d='welcome to python'
+# print(d)
+# x=(d[4:10])
+# print(x)
+#
+# t=(1,"one"),(2,"two"),(3,"three")
+# print(t)
+# a=dict(t)
+# print(a)
+#
+# str='welcome to python'
+# a=str[::-1]
+# print(a)
+# str='welcome to python'
+# a=str[::-2]
+# print(a)
+# a=str[::-3]
+# print(a)
+#
+# l=[10,5,4,20,1,15,12]
+# l.sort()
+# print(l)
+#
+# l=["one","two","three"]
+# s=[1,2,3]
+# mydict=dict(zip(l,s))
+# print(mydict)
+#
+#list = [22, 44, 66, 56, 77, 89]
+#l=[]
+#for i in list:
+#     #if i%2 ==0:
+#         l.append(i)
+# print(l)
+
+# l=[]
+# for i in range(2000 ,3201):
+#     if (i%7==0) and (i%5!=0):
+#         l.append(str(i))
+# #print(l,end=",")
+# #
+# str=("rajeghegsjh")
+# #print(len(str))
+# #
+# str="12344"
+# print(int(str))
+# #print(float(str))
+# #
+# l=[1,2,4,5,6,7,8,9]
+# print(l)
+# #print(tuple(l))
+# t=(1,2,3,4,5,66,7,8,9)
+# print(t)
+# #print(list(t))
+# dict={"name":"raji","age":"24","year":"1997"}
+# print(dict)
+# print(dict.keys())
+# print(dict.values())
+# #
+# l=[11,32,43,44,55,66,76]
+# print(l)
+# l[2]=95
+# print(l)
+# t=(11,23,44,55,66,77)
+# print(t)
+# #
+# #
+# d='welcome to python'
+# print(d)
+# x=(d[4:10])
+# print(x)
+#
+# t=(1,"one"),(2,"two"),(3,"three")
+# print(t)
+# a=dict(t)
+# print(a)
+#
+# str='welcome to python'
+# a=str[::-1]
+# print(a)
+# str='welcome to python'
+# a=str[::-2]
+# print(a)
+# a=str[::-3]
+# print(a)
+#
+# l=[10,5,4,20,1,15,12]
+# l.sort()
+# print(l)
+#
+# l=["one","two","three"]
+# s=[1,2,3]
+# mydict=dict(zip(l,s))
+# print(mydict)
+#
+#list = [22, 44, 66, 56, 77, 89]
+#l=[]
+#for i in list:
+#     #if i%2 ==0:
+#         l.append(i)
+# print(l)
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
